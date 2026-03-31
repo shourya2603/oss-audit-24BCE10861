89bff4fa28e85ece0c8ae28096b862c57c9c9663
shourya2603 <shourya26032007@gmail.com>
2026-03-31 22:39:19 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -2,7 +2,7 @@
 # Script 1: System Identity Report
 # Author: [Your Name] | Course: Open Source Software
 # --- Variables ---
-STUDENT_NAME="Himanshu Singh" # Fill in your name
+STUDENT_NAME="Shourya Mishra" # Fill in your name
 SOFTWARE_CHOICE="Linux Kernel" # Fill in your chosen software
 # --- System info ---
 KERNEL=$(uname -r)
